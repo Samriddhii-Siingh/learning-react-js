9195cb851e89ee015ff81dffb3feaa384e5eac0d
Samriddhi <ss2072163@gmail.com>
2023-02-11 23:47:53 +0530

    more jokes edit

--- portfolioFromScratch/dist/index.975ef6c8.js
+++ portfolioFromScratch/dist/index.975ef6c8.js
@@ -28413,9 +28413,39 @@ parcelHelpers.defineInteropFlag(exports);
 var _jsxDevRuntime = require("react/jsx-dev-runtime");
 var _react = require("react");
 var _reactDefault = parcelHelpers.interopDefault(_react);
+// in place of props as I/P, joke as I/P 
+// const Joke = ({joke})=> {
+// const{ setup, punchline} = joke;
+// return <p style={{ marging: 20 }}> {setup} <em>{punchline}</em> </p>
+// }
+const Joke = ({ joke: { setup , punchline  }  })=>/*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("p", {
+        style: {
+            marging: 20
+        },
+        children: [
+            " ",
+            setup,
+            " ",
+            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("em", {
+                children: punchline
+            }, void 0, false, {
+                fileName: "src/components/Jokes.js",
+                lineNumber: 10,
+                columnNumber: 41
+            }, undefined),
+            " "
+        ]
+    }, void 0, true, {
+        fileName: "src/components/Jokes.js",
+        lineNumber: 10,
+        columnNumber: 5
+    }, undefined);
+_c = Joke;
 class Jokes extends (0, _react.Component) {
+    // array for 10 jokes 
     state = {
-        joke: {}
+        joke: {},
+        jokes: []
     };
     // place fetch (& other asynchronous code) in componentDidMount()
     // fetch is asynchronous, don't know duration of loading
@@ -28431,42 +28461,70 @@ class Jokes extends (0, _react.Component) {
                 joke: json
             }));
     }
+    fetchJokes = ()=>{
+        fetch("https://official-joke-api.appspot.com/random_ten").then((response)=>response.json()).then((json)=>this.setState({
+                jokes: json
+            }));
+    };
     render() {
-        const { setup , punchline  } = this.state.joke;
+        // const { setup, punchline } = this.state.joke;
         return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
             children: [
                 /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h2", {
                     children: "Highlighted Joke"
                 }, void 0, false, {
                     fileName: "src/components/Jokes.js",
-                    lineNumber: 26,
+                    lineNumber: 43,
                     columnNumber: 17
                 }, this),
-                /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("p", {
-                    children: [
-                        setup,
-                        /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("em", {
-                            children: punchline
-                        }, void 0, false, {
-                            fileName: "src/components/Jokes.js",
-                            lineNumber: 27,
-                            columnNumber: 27
-                        }, this)
-                    ]
-                }, void 0, true, {
+                /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)(Joke, {
+                    joke: this.state.joke
+                }, void 0, false, {
                     fileName: "src/components/Jokes.js",
-                    lineNumber: 27,
+                    lineNumber: 45,
                     columnNumber: 17
-                }, this)
+                }, this),
+                /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("hr", {}, void 0, false, {
+                    fileName: "src/components/Jokes.js",
+                    lineNumber: 46,
+                    columnNumber: 17
+                }, this),
+                /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h3", {
+                    children: "Want ten new jokes?"
+                }, void 0, false, {
+                    fileName: "src/components/Jokes.js",
+                    lineNumber: 47,
+                    columnNumber: 17
+                }, this),
+                /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("button", {
+                    onClick: this.fetchJokes,
+                    children: "Click me!"
+                }, void 0, false, {
+                    fileName: "src/components/Jokes.js",
+                    lineNumber: 48,
+                    columnNumber: 17
+                }, this),
+                this.state.jokes.map((joke)=>// const { id, setup, punchline} = joke;
+                    // return <p key={id}> {setup} <em>{punchline}</em></p>
+                    // using inline return
+                    /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)(Joke, {
+                        joke: joke
+                    }, joke.id, false, {
+                        fileName: "src/components/Jokes.js",
+                        lineNumber: 55,
+                        columnNumber: 25
+                    }, this))
             ]
         }, void 0, true, {
             fileName: "src/components/Jokes.js",
-            lineNumber: 25,
+            lineNumber: 42,
             columnNumber: 13
         }, this);
     }
 }
 exports.default = Jokes;
+var _c;
+$RefreshReg$(_c, "Joke");
 
   $parcel$ReactRefreshHelpers$40f0.postlude(module);
 } finally {
--- portfolioFromScratch/src/components/Jokes.js
+++ portfolioFromScratch/src/components/Jokes.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
 
+// in place of props as I/P, joke as I/P 
+// const Joke = ({joke})=> {
+    // const{ setup, punchline} = joke;
+    // return <p style={{ marging: 20 }}> {setup} <em>{punchline}</em> </p>
+// }
+
+const Joke = ({joke: { setup, punchline}}) => (
+    <p style={{ marging: 20 }}> {setup} <em>{punchline}</em> </p>
+)
+
 class Jokes extends Component {
-    state = { joke: {} };
+    // array for 10 jokes 
+    state = { joke: {}, jokes: []};
 
     // place fetch (& other asynchronous code) in componentDidMount()
     // fetch is asynchronous, don't know duration of loading
@@ -19,12 +30,31 @@ class Jokes extends Component {
 
     }
 
+    fetchJokes = () => {
+        fetch('https://official-joke-api.appspot.com/random_ten')
+            .then(response => response.json())
+            .then(json => this.setState({jokes: json}));
+    }
+
     render() {
-        const { setup, punchline } = this.state.joke;
+        // const { setup, punchline } = this.state.joke;
         return (
             <div>
                 <h2>Highlighted Joke</h2>
-                <p>{setup}<em>{punchline}</em></p>
+                {/* <p>{setup}<em>{punchline}</em></p> */}
+                <Joke joke={this.state.joke} />
+                <hr/>
+                <h3>Want ten new jokes?</h3>
+                <button onClick={this.fetchJokes}>Click me!</button>
+                {
+                    this.state.jokes.map(joke => (
+                        // const { id, setup, punchline} = joke;
+                        // return <p key={id}> {setup} <em>{punchline}</em></p>
+
+                        // using inline return
+                        <Joke key={joke.id} joke={joke} />
+                    ))
+                }
             </div>
         )
     }
